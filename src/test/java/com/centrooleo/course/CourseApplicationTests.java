00a6752817bba72d1982239e1dda7add4998c0d6
Guilherme Morbeck <morbeck719@gmail.com>
2022-08-23 13:49:16 -0300

    Project created

---
+++ src/test/java/com/centrooleo/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centrooleo.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
