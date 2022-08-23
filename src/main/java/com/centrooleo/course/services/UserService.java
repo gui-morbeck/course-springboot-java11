5004d02e19b091bb41ae122aa2839e545fc59406
Guilherme Morbeck <morbeck719@gmail.com>
2022-08-23 19:36:04 -0300

    Service layer, component registration

---
+++ src/main/java/com/centrooleo/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.centrooleo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.centrooleo.course.entities.User;
+import com.centrooleo.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+	
+}
