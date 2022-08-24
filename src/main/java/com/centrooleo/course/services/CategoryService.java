f130674aeeecc50f7ae614942917dd793b74dc5b
Guilherme Morbeck <morbeck719@gmail.com>
2022-08-24 12:25:56 -0300

    Category

---
+++ src/main/java/com/centrooleo/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.centrooleo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.centrooleo.course.entities.Category;
+import com.centrooleo.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository catRepository;
+	
+	public List<Category> findAll(){
+		List<Category> list = catRepository.findAll();
+		return list;
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = catRepository.findById(id);
+		return obj.get();
+	}
+
+}
